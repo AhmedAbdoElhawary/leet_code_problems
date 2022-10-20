9d7a9d389fffb07e6e844fbc8d09ea0fb6c7b991
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2022-10-20 18:26:50 +0200

    Time: 309 ms (5.09%), Space: 51.5 MB (40.61%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> hash;
+        
+        for(int i=0;i<nums.size();i++){
+            if(hash.find(nums[i])!=hash.end())
+                return true;
+            hash.insert(nums[i]);
+        }
+        return false;
+    }
+};
